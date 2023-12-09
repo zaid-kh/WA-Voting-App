5c8725f3f141eda2458094afc7e426e872f403e9
zaid-kh <z.kh2800@gmail.com>
2023-12-09 19:33:47 +0200

    add table to admin page -without styling-

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getUsers } from "../../APIs/users";
 
 export const Admin = () => {
-  return <div>Admin page</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setUsers(await getUsers());
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <h1>Admin Page</h1>
+      <section className="users-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Voted</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users ? (
+              users.map((user) => {
+                return <UserTableRow user={user} />;
+              })
+            ) : (
+              <tr>
+                <td colSpan="3">No users found!</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </section>
+    </>
+  );
 };
+
+function UserTableRow({ user }) {
+  return (
+    <tr>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+      <td>{user.voted ? "Voted" : "Not Voted"}</td>
+    </tr>
+  );
+}
